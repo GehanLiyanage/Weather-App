6120250092c2bcda4339b65e4723a7a5f4d252b1
Gehan Liyanage <108855388+GehanLiyanage@users.noreply.github.com>
2024-12-23 07:45:10 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
-"transform": {
-  "^.+\\.jsx?$": "babel-jest"
-}
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Optional for custom Jest setups
+  moduleNameMapper: {
+    '\\.(css|scss|less)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  collectCoverage: true, // Optional: Enable code coverage
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/index.js'], // Adjust per project
+};
